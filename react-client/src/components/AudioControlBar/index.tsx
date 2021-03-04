1cd2764e3d13aec61bf9bdf2907d74dfc3d10044
Jan Radziminski <radziminski.j@gmail.com>
2021-03-04 22:14:14 +0100

    Refactored everything

--- react-client/src/components/AudioControlBar/index.tsx
+++ react-client/src/components/AudioControlBar/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from './parts';
 import { useStoreActions, useStoreState } from 'global-state/hooks';
 import Icon from 'components/Icon';
 import Box from 'components/Box';
 import AudioTimer from 'components/AudioTimer';
+import VolumeSlider from 'components/VolumeSlider';
 
 export const AudioControlBar: React.FC = () => {
+  const [gainValue, setGainValue] = useState(1);
   const { controller } = useStoreState((state) => state.audio);
   const { play, pause } = useStoreActions((actions) => actions.audio);
 
@@ -19,7 +21,18 @@ export const AudioControlBar: React.FC = () => {
           <Icon size={34} icon={'play-circle'} onClick={() => play()} />
         )}
       </Box>
-      <Box width={100}></Box>
+      <Box width={200} overflow='hidden'>
+        <VolumeSlider
+          min={0}
+          max={1}
+          value={gainValue}
+          step={0.05}
+          onChange={(value) => {
+            if (controller) controller.masterGainNode.gain.value = value;
+            setGainValue(value);
+          }}
+        />
+      </Box>
     </Container>
   );
 };
