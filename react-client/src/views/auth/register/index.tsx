f3a615bba621ae6c0ce420ffc50484f25c2c2b79
Jan Radziminski <radziminski.j@gmail.com>
2021-05-04 22:01:35 +0200

    Added register page

--- react-client/src/views/auth/register/index.tsx
+++ react-client/src/views/auth/register/index.tsx
@@ -1,7 +1,32 @@
+import Box, { FlexBox } from 'components/Box';
+import { Heading2, Heading3, Paragraph } from 'components/Text';
 import React from 'react';
+import { FONT_WEIGHTS } from 'styles/theme';
+import RegisterForm from './parts';
 
 export const RegisterView: React.FC = () => {
-  return <div></div>;
+  return (
+    <FlexBox flexDirection='column' maxWidth='600px'>
+      <Heading2 fontSize='2rem' fontWeight={FONT_WEIGHTS.medium}>
+        Sign up
+      </Heading2>
+
+      <Box marginTop='4rem'>
+        <Heading3 fontSize='1.2rem' fontWeight={FONT_WEIGHTS.semiBold}>
+          Create new account
+        </Heading3>
+      </Box>
+
+      <Box marginTop='2rem' marginBottom='1rem'>
+        <Paragraph fontSize='1rem'>
+          Welcome to Audio Analyser! Please provide your email and data to
+          create a new account.
+        </Paragraph>
+      </Box>
+
+      <RegisterForm />
+    </FlexBox>
+  );
 };
 
 export default RegisterView;
---
+++ react-client/src/views/auth/register/parts.tsx
@@ -0,0 +1,104 @@
+import { ActionButton } from 'components/ActionButton';
+import Box, { FlexBox } from 'components/Box';
+import Form from 'components/Form';
+import TextInput from 'components/TextInput';
+import React, { useEffect, useState } from 'react';
+import { useStoreActions, useStoreState } from 'global-state/hooks';
+import { useHistory } from 'react-router';
+import { ROUTES } from 'constants/routes';
+import { Heading3 } from 'components/Text';
+import { FONT_WEIGHTS } from 'styles/theme';
+
+export const RegisterForm: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [repeatPassword, setRepeatPassword] = useState<string>('');
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+
+  const history = useHistory();
+
+  const { authError, isLoading, isAuthenticated } = useStoreState(
+    (store) => store.auth
+  );
+  const { register } = useStoreActions((store) => store.auth);
+
+  const onSubmit = async () => {
+    // void register({ email, password });
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) history.push(ROUTES.DASHBOARD);
+  }, [isAuthenticated, history]);
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <Box height='1rem'>
+        {authError && (
+          <Heading3
+            fontSize='0.8rem'
+            color='red'
+            fontWeight={FONT_WEIGHTS.medium}
+          >
+            {authError}
+          </Heading3>
+        )}
+      </Box>
+      <Box marginTop='1rem'>
+        <TextInput
+          type='text'
+          label='Email*'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='Ex. john@gmail.com'
+        />
+      </Box>
+      <FlexBox marginTop='2rem' justifyContent='space-between'>
+        <Box width='48%'>
+          <TextInput
+            type='password'
+            label='Password*'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='Strong Password'
+          />
+        </Box>
+        <Box width='48%'>
+          <TextInput
+            type='password'
+            label='Repeat Password*'
+            value={repeatPassword}
+            onChange={(e) => setRepeatPassword(e.target.value)}
+            placeholder='Repeat password'
+          />
+        </Box>
+      </FlexBox>
+      <FlexBox marginTop='2rem' justifyContent='space-between'>
+        <Box width='48%'>
+          <TextInput
+            type='text'
+            label='First Name'
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            placeholder='Ex. John'
+          />
+        </Box>
+        <Box width='48%'>
+          <TextInput
+            type='text'
+            label='Last Name'
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            placeholder='Ex. Smith'
+          />
+        </Box>
+      </FlexBox>
+
+      <Box marginTop='4rem'>
+        <ActionButton isLoading={isLoading}>Login</ActionButton>
+      </Box>
+    </Form>
+  );
+};
+
+export default RegisterForm;
